ff30625763416b9cb6105793653b6c800516869d
Jaroslav Bažant <jardabazant@seznam.cz>
2017-10-02 11:59:33 +0200

    Basci pokemon class added with basic parametres

---
+++ Pokedex3/Model/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  Pokedex3
+//
+//  Created by Jaroslav Bažant on 02.10.17.
+//  Copyright © 2017 DewDrops. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
